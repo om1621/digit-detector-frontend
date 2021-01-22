a8509eb006172d9d32658e338a7e6b3ea31e49f2
Onkar Telange <Onkartelange2000@gmail.com>
2021-01-22 20:36:42 +0530

    added reset and predict functionality

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ $(document).ready(() => {
     
     const canvas = $('#canvas')[0];
     const ctx = canvas.getContext("2d");
-
-    // canvas.width = $('.input-box').width() * 1;
-    // canvas.height = $('body').height() * 0.5;
-
-    // $(window).resize(function () { 
-    //    canvas.width = $('.input-box').width()*1;
-    //    canvas.height = $('body').height() * 0.5;
-    // });
    
     let painting = false;
 
@@ -32,20 +24,32 @@ $(document).ready(() => {
     canvas.addEventListener('mousedown', (e) => {
         painting = true;
         draw(e);
-    })
+    });
 
     canvas.addEventListener('mouseup', () => {
         painting = false;
         ctx.beginPath();
-        const data = canvas.toDataURL();
-        $('.output-image').attr('src', data);
-        $('.output-image').css('display', 'block');
-
-    })
+    });
 
     canvas.addEventListener('mousemove', (e) => {
         draw(e);
-    })
+    });
+
+    $('#predict').click(function (e) { 
+        e.preventDefault();
+        const data = canvas.toDataURL();
+        $('.output-image').attr('src', data);
+        $('.output-image').css('display', 'block');
+        
+    });
+
+    $('#reset').click(function (e) { 
+        e.preventDefault();
+        $('.output-image').css('display', 'none');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        
+    });
 
 
     
